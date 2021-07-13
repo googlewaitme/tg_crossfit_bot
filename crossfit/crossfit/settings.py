9b52123275f1b81f2effa00229a9034a9c55d156
DobryiMrak <kusok.apelbsina@hotmail.com>
2021-07-13 15:46:38 +0500

    обновил настройки для работы с асинхронным aiogram

--- crossfit/crossfit/settings.py
+++ crossfit/crossfit/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from environs import Env
+
+
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+env = Env()
+env.read_env('.env.test')
+
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMINS = env.list("ADMINS")
+IP = env.str("ip")
