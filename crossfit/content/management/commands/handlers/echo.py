d327aae2c4b8ab65abb2ae26fb9ccc2cd5e5e114
DobryiMrak <kusok.apelbsina@hotmail.com>
2021-07-13 17:23:25 +0500

    Добавил заглушку для колбеков, которые не было отловлены

--- crossfit/content/management/commands/handlers/echo.py
+++ crossfit/content/management/commands/handlers/echo.py
@@ -2,6 +2,13 @@ from aiogram import types
 from ..loader import dp
 
 
+@dp.callback_query_handler(lambda c: True)
+async def send_echo(callback_query: types.CallbackQuery):
+    await callback_query.message.edit_text(f'Колбек без хендлера'
+                               f'Колбек:'
+                               f'{callback_query.data}')
+
+
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
     await message.answer(f"Эхо без состояния."
