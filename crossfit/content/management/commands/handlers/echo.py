e2c0dbe97dfa298007cca18372caf5a45b745725
DobryiMrak <kusok.apelbsina@hotmail.com>
2021-07-13 20:17:58 +0500

    Теперь нормально отображаются категории с одной или двуми публикациями

--- crossfit/content/management/commands/handlers/echo.py
+++ crossfit/content/management/commands/handlers/echo.py
@@ -5,8 +5,8 @@ from ..loader import dp
 @dp.callback_query_handler(lambda c: True)
 async def send_echo(callback_query: types.CallbackQuery):
     await callback_query.message.edit_text(f'Колбек без хендлера'
-                               f'Колбек:'
-                               f'{callback_query.data}')
+                                           f'Колбек:'
+                                           f'{callback_query.data}')
 
 
 @dp.message_handler(state=None)
