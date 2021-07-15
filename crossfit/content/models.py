ff6e2f18e72e201e823e260d0bfb04034e4363ea
DobryiMrak <kusok.apelbsina@hotmail.com>
2021-07-15 12:40:42 +0500

    Добавил новую модель пользователей

--- crossfit/content/models.py
+++ crossfit/content/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+class TelegramUser(models.Model):
+    telegram_id = models.CharField(editable=False, max_length=12)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.telegram_id
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
 class AbstractCategory(models.Model):
     name = models.CharField(max_length=200, verbose_name='Имя категории')
     description = models.TextField(verbose_name='Описание')
