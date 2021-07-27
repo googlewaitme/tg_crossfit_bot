9dd9f1ee41d402f9379de5487c1c051ccee204b0
DobryiMrak <kusok.apelbsina@hotmail.com>
2021-07-28 00:27:01 +0500

    Сделал нужные мне модели и заставил их работать, как мне нужно

--- crossfit/gym_aggregator/admin.py
+++ crossfit/gym_aggregator/admin.py
@@ -4,4 +4,27 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(GymProfile)
+class GymProfileInline(admin.TabularInline):
+    model = GymProfile.locations.through
+    model._meta.verbose_name_plural = "Локации"
+    model._meta.verbose_name = 'Локация'
+    extra = 1
+
+
+class RaitingInline(admin.StackedInline):
+    model = Raiting
+
+
+class GymProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+    inlines = (GymProfileInline, RaitingInline)
+    fieldsets = [
+        ('', {
+            'fields': ('name', 'address', 'general_comment')
+        })
+    ]
+
+
+admin.site.register(GymProfile, GymProfileAdmin)
+admin.site.register(City)
+admin.site.register(Location)
--- crossfit/gym_aggregator/models.py
+++ crossfit/gym_aggregator/models.py
@@ -2,6 +2,33 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 
+class City(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя города')
+    is_hidden = models.BooleanField(
+        default=False, verbose_name='Отображение')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+
+class Location(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name='Обозначение локации')
+    city = models.ForeignKey(
+        City, verbose_name='Город', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Локация'
+        verbose_name_plural = 'Локации'
+
+
 def validate_grade(value):
     if 0 <= value <= 10:
         return value
@@ -17,7 +44,29 @@ def grade_field(verbose_name, default=0):
     return field
 
 
+class GymProfile(models.Model):
+    name = models.CharField(max_length=300, verbose_name='Название')
+    address = models.CharField(max_length=300, verbose_name='Адресс')
+    general_comment = models.TextField(verbose_name='Общий комментарий')
+    locations = models.ManyToManyField(
+        Location,
+        verbose_name='Локации'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Спортзал'
+        verbose_name_plural = 'Спортзалы'
+
+
 class Raiting(models.Model):
+    raiting = models.OneToOneField(
+        GymProfile,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     location = grade_field('Местоположение')
     square = grade_field('Площадь зала')
     equipment = grade_field('Оснащённость зала')
@@ -28,13 +77,6 @@ class Raiting(models.Model):
     convenience = grade_field('Сопутствующие блага')
     service = grade_field('Сервис')
 
-
-class GymProfile(models.Model):
-    name = models.CharField(max_length=300, verbose_name='Название')
-    address = models.CharField(max_length=300, verbose_name='Адресс')
-    general_comment = models.TextField(verbose_name='Общий комментарий')
-    raiting = models.OneToOneField(
-        Raiting,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
+    class Meta:
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
