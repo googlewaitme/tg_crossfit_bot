8ef6e1baaee6373f13e074ca54df5acfca09080c
DobryiMrak <kusok.apelbsina@hotmail.com>
2021-08-05 06:42:23 +0500

    Сделал так, чтобы в меню можно было попасть из любого состояния, и оно очищало fsm пользователя

--- crossfit/content/management/commands/handlers/menu.py
+++ crossfit/content/management/commands/handlers/menu.py
@@ -2,13 +2,14 @@ from ..loader import dp
 from aiogram import types
 from ..keyboards.inline import menu_markup
 from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher import FSMContext
 from content.models import TelegramUser
-from ..utils.filters import starts_with
 from ..utils.helpers import get_message_one_button
 
 
-@dp.message_handler(Command('menu'))
-async def send_menu(message: types.Message):
+@dp.message_handler(Command('menu'), state='*')
+async def send_menu(message: types.Message, state: FSMContext):
+    await state.reset_state()
     user, created = TelegramUser.objects.get_or_create(
         telegram_id=message.from_user.id)
     text = get_message_one_button('MENU_MESSAGE')
@@ -16,8 +17,9 @@ async def send_menu(message: types.Message):
                          reply_markup=menu_markup.get())
 
 
-@dp.callback_query_handler(starts_with('menu'))
-async def send_menu_by_callback(callback_query: types.CallbackQuery):
+@dp.callback_query_handler(text='menu', state='*')
+async def send_menu_by_callback(callback_query: types.CallbackQuery, state: FSMContext):
+    await state.reset_state()
     user, created = TelegramUser.objects.get_or_create(
         telegram_id=callback_query.from_user.id)
     text = get_message_one_button('MENU_MESSAGE')
