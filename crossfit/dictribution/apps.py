2aa24e63ea9fa4a7c3ee8b9623165cf0de5379c7
DobryiMrak <kusok.apelbsina@hotmail.com>
2021-08-05 16:40:04 +0500

    Изменил сортировку в django adminэ

--- crossfit/dictribution/apps.py
+++ crossfit/dictribution/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DictributionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dictribution'
+    verbose_name = '3) Рассылка'
--- crossfit/gym_aggregator/apps.py
+++ crossfit/gym_aggregator/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class GymAggregatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gym_aggregator'
+    verbose_name = '2) Агрегатор залов'
