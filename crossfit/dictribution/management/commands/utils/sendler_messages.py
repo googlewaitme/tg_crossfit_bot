ac9475f4c40648194c5d1c9e5d9c9f7e917b5e0a
DobryiMrak <kusok.apelbsina@hotmail.com>
2021-08-10 19:42:30 +0500

    Исправил ошибку с рассылкой

--- crossfit/dictribution/management/commands/utils/sendler_messages.py
+++ crossfit/dictribution/management/commands/utils/sendler_messages.py
@@ -1,9 +1,7 @@
 from content.management.commands.loader import dp
 from .make_message import DictributionMessage
 import time
-from contextlib import suppress
 from content.models import TelegramUser
-from aiogram.utils.exceptions import ChatNotFound
 import logging
 
 
@@ -11,7 +9,9 @@ async def send_dictribution(dictribution):
     logging.info(f'start dictribution: {dictribution.name}')
     message = DictributionMessage(dictribution).get_as_python()
     for user in TelegramUser.objects.all():
-        with suppress(ChatNotFound):
+        try:
             await dp.bot.send_message(chat_id=user.telegram_id, **message)
             time.sleep(0.5)
+        except:
+            pass
     logging.info(f'end dictribution: {dictribution.name}')
